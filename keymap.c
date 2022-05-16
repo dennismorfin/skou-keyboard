665ebe39736c3fec7721ae4f69387963eb367dc5
dennismorfin <skou259@gmail.com>
2022-05-16 17:30:11 -0500

    progress

--- keymap.c
+++ keymap.c
@@ -16,7 +16,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //---------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
      TD(TD_CAPLOCK), KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,                 KC_N,    KC_M,      KC_COMM, KC_DOT,  TD(TD_SLA), RGB_MOD,
   //---------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                KC_LGUI, LT(_LOWER,KC_ENT),  DOFUS,     KC_SPC, LT(_RAISE,KC_ENT), TD(TD_ALT)
+                                KC_LGUI, LT(_LOWER,KC_ENT),  KC_LSFT,     KC_SPC, LT(_RAISE,KC_ENT), TD(TD_ALT)
                                       //|--------------------------|  |--------------------------|
   ),
 
